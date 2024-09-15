93c8f7bb121c7c4c4716106abe494e8c8ff963ae
godjr13 <mdinusha11@gmail.com>
2024-09-16 00:22:02 +0530

    ...

--- floodfill.ino
+++ floodfill.ino
@@ -11,25 +11,28 @@ int tof_sp, irleft_sp, irright_sp, irr45_sp, irl45_sp;
 
 int cell_val; //current cell value (set starting cell as current cell in the begining..)
 int current_cell[2] = {0,16}; 
+int start[2] = {0,16};
+int goal[2] = {8,8};
 
 //maze matrix
 int maze[16][16] = {
-            {14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14},
-            {13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13},
-            {12,11,10,9,8,7,6,5,5,6,7,8,9,10,11,12},
-            {11,10,9,8,7,6,5,4,4,5,6,7,8,9,10,11},
-            {10,9,8,7,6,5,4,3,3,4,5,6,7,8,9,10},
-            {9,8,7,6,5,4,3,2,2,3,4,5,6,7,8,9},
-            {8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8},
-            {7,6,5,4,3,2,1,0,0,1,2,3,4,5,6,7},
-            {7,6,5,4,3,2,1,0,0,1,2,3,4,5,6,7},
-            {8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8},
-            {9,8,7,6,5,4,3,2,2,3,4,5,6,7,8,9},
-            {10,9,8,7,6,5,4,3,3,4,5,6,7,8,9,10},
-            {11,10,9,8,7,6,5,4,4,5,6,7,8,9,10,11},
-            {12,11,10,9,8,7,6,5,5,6,7,8,9,10,11,12},
-            {13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13},
-            {14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14}};
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};//zero represents open paths and 1 represents closed paths
 
 //manhattan distance maze
 int manhattan_maze[16][16] = {
@@ -70,29 +73,44 @@ int explored_cells[16][16] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 };
 
+int walls[16][16] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
 //movement
-void move_up(){
-   
-  //update current cell
-  current_cell[1]++;
-}
+void move(int direction){
+  switch(direction){
+    case 1://up 
+      current_cell[1]++;
+      break;
+    case 2: //down 
+      current_cell[1]--;
+      break;
+    case 3: //left 
+      current_cell[0]--;
+      break;
+    case 4: //right
+      current_cell[0]++;
+      break;
+  }
 
-void move_left(){
-
-  //update current cell
-  current_cell[0]--;
-}
-
-void move_right(){
-  
-  //update current cell
-  current_cell[0]++;
-}
-
-void move_down(){
-  
-  //update current cell
-  current_cell[1]--;
+  //marking explored cells
+  explored_cells[current_cell[0]][current_cell[1]] = 1;
 }
 
 //other functions
@@ -107,7 +125,6 @@ int check_wall(){
     Scenario 6: wall left and front
     Scenario 7: wall right and front
     Scenario 8: wall left,right and front */
-  
   int wall_val;
   return wall_val;
 }
@@ -148,7 +165,7 @@ void update_explored(){
 
 int main(){
   //searching algorithm
-  if(cell_val != 0){
+  while(current_cell != goal){
     int wall = check_wall();
     
     switch(wall){
@@ -157,24 +174,36 @@ int main(){
         break;
 
       case 2: //wall left
+        maze[current_cell[0]-1] [current_cell[1]] = 1; //set the left cell as a closed path
         break;
 
       case 3: //wall right
+        maze[current_cell[0]+1] [current_cell[1]] = 1;
         break;
 
       case 4: //wall front
+        maze[current_cell[0]] [current_cell[1]+1] = 1;
         break;
 
       case 5: //wall left and right
+        maze[current_cell[0]-1] [current_cell[1]] = 1;
+        maze[current_cell[0]+1] [current_cell[1]] = 1;
         break;
 
       case 6: //wall left and front
+        maze[current_cell[0]] [current_cell[1]+1] = 1;
+        maze[current_cell[0]-1] [current_cell[1]] = 1;
         break;
 
       case 7: //wall right and front
+        maze[current_cell[0]+1] [current_cell[1]] = 1;
+        maze[current_cell[0]] [current_cell[1]+1] = 1;
         break;
 
       case 8: //wall left,right and front
+        maze[current_cell[0]] [current_cell[1]+1] = 1;
+        maze[current_cell[0]-1] [current_cell[1]] = 1;
+        maze[current_cell[0]+1] [current_cell[1]] = 1;
         break;
 
     }
